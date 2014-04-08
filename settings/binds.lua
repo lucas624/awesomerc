6a36b99584a452e48f5bd7e4c20ef0d785644c7a
Lucas Traverso <lucas6246@gmail.com>
2014-04-08 14:41:58 -0300

    Added bind for toggle touchpad

--- settings/binds.lua
+++ settings/binds.lua
@@ -36,6 +36,10 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey,           }, "q", function()
+        local fd = io.popen("if [ $(synclient -l | grep TouchpadOff | awk '{print $3}') == 1 ] ; then synclient touchpadoff=0; else synclient touchpadoff=1; fi")
+        fd:close()
+    end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
