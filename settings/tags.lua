efe579745613980afa1fa87907c78eb29df1758b
Lucas David Traverso <lucas6246@gmail.com>
2014-08-02 03:05:12 -0300

    Stylize everything with tabularize

--- settings/tags.lua
+++ settings/tags.lua
@@ -20,87 +20,83 @@ layouts = {
 
  tyrannical.tags = {
     {
-        name        = "www",
-        init        = true,
-      --exclusive   = true,
-      --icon        = "~net.png",                 -- Use this icon for the tag (uncomment with a real path)
-        screen      = screen.count()>1 and 2 or 1,-- Setup on screen 2 if there is more than 1 screen, else on screen 1
-        layout      = awful.layout.suit.floating,      -- Use the max layout
-        class = {
-            "Opera"         , "Firefox"        , "Rekonq"    , "Dillo"        , "Arora",
-            "Chromium"      , "nightly"        , "Dwb"       , "Google-chrome-stable"}
-    } ,
-    {
-        name        = "term",                               -- Call the tag "Term"
-        init        = false,                                -- Load the tag on startup
-        exclusive   = true,                                 -- Refuse any other type of clients (by classes)
-        screen      = {1,2},                                -- Create this tag on screen 1 and screen 2
-        layout      = awful.layout.suit.fair.horizontal,    -- Use the tile layout
-        class       = { --Accept the following classes, refuse everything else (because of "exclusive=true")
-            "xterm" , "urxvt" , "aterm","URxvt","XTerm","konsole","terminator","gnome-terminal",
-            "Xfce4-terminal"
+        name      = "www",
+        init      = true,
+        screen    = screen.count()>1 and 2 or 1, -- Setup on screen 2 if there is more than 1 screen, else on screen 1
+        layout    = awful.layout.suit.floating,  -- Use the max layout
+        class     = {
+            "Opera"    , "Firefox" , "Rekonq" , "Dillo"                , "Arora" ,
+            "Chromium" , "nightly" , "Dwb"    , "Google-chrome-stable"
         }
     } ,
     {
-        name        = "files",
-        init        = false,
-      --exclusive   = true,
-        screen      = 1,
-        layout      = awful.layout.suit.tile,
-      --exec_once   = {"dolphin"}, --When the tag is accessed for the first time, execute this command
-        class  = {
+        name      = "term",                            -- Call the tag "Term"
+        init      = false,                             -- Load the tag on startup
+        exclusive = false,                             -- Refuse any other type of clients (by classes)
+        screen    = {1,2},                             -- Create this tag on screen 1 and screen 2
+        layout    = awful.layout.suit.fair.horizontal, -- Use the tile layout
+        class     = {                                  -- Accept the following classes, refuse everything else (because of "exclusive        = true")
+            "xterm"   , "urxvt"      , "aterm"          , "URxvt"          , "XTerm" ,
+            "konsole" , "terminator" , "gnome-terminal" , "Xfce4-terminal"
+        }
+    } ,
+    {
+        name      = "files",
+        init      = false,
+        exclusive = false,
+        screen    = 1,
+        layout    = awful.layout.suit.tile,
+        exec_once = {}, --When the tag is accessed for the first time, execute this command
+        class     = {
             "Thunar", "Konqueror", "Dolphin", "ark", "Nautilus","emelfm"
         }
     } ,
     {
-        name        = "develop",
-        init        = false,
-        exclusive   = true,
-        screen      = 1,
-        clone_on    = 2, -- Create a single instance of this tag on screen 1, but also show it on screen 2
-                         -- The tag can be used on both screen, but only one at once
-        layout      = awful.layout.suit.max                          ,
-        class ={ 
+        name      = "develop",
+        init      = false,
+        exclusive = true,
+        screen    = 1,
+        clone_on  = 2, -- Create a single instance of this tag on screen 1, but also show it on screen 2
+        layout    = awful.layout.suit.max                          ,
+        class     = {
             "Kate", "KDevelop", "Codeblocks", "Code::Blocks" , "DDD", "kate4", "Eclipse"}
     } ,
     {
-        name        = "doc",
-        init        = false, -- This tag wont be created at startup, but will be when one of the
-                             -- client in the "class" section will start. It will be created on
-                             -- the client startup screen
-        exclusive   = true,
-        layout      = awful.layout.suit.max,
-        class       = {
+        name      = "doc",
+        init      = false,
+        exclusive = true,
+        layout    = awful.layout.suit.max,
+        class     = {
             "Assistant"     , "Okular"         , "Evince"    , "EPDFviewer"   , "Xpdf" }
     }
 }
 
 -- Ignore the tag "exclusive" property for the following clients (matched by classes)
 tyrannical.properties.intrusive = {
-    "ksnapshot"     , "pinentry"       , "gtksu"     , "kcalc"        , "xcalc"               ,
-    "feh"           , "Gradient editor", "About KDE" , "Paste Special", "Background color"    ,
-    "kcolorchooser" , "plasmoidviewer" , "Xephyr"    , "kruler"       , "plasmaengineexplorer",
+    "ksnapshot"     , "pinentry"        , "gtksu"     , "kcalc"         , "xcalc"                ,
+    "feh"           , "Gradient editor" , "About KDE" , "Paste Special" , "Background color"     ,
+    "kcolorchooser" , "plasmoidviewer"  , "Xephyr"    , "kruler"        , "plasmaengineexplorer" ,
     "Qalculate-gtk" , "Pavucontrol"
 }
 
 -- Ignore the tiled layout for the matching clients
 tyrannical.properties.floating = {
-    "pinentry"        , "ksnapshot"  , "pinentry"     , "gtksu"          ,"Qalculate-gtk" ,
-    "xine"         , "feh"             , "kmix"       , "kcalc"        , "xcalc"          ,
-    "yakuake"      , "Select Color$"   , "kruler"     , "kcolorchooser", "Paste Special"  ,
-    "New Form"     , "Insert Picture"  , "kcharselect", "mythfrontend" , "plasmoidviewer" ,
+    "pinentry"    , "ksnapshot"      , "pinentry"    , "gtksu"         , "Qalculate-gtk"  ,
+    "xine"        , "feh"            , "kmix"        , "kcalc"         , "xcalc"          ,
+    "yakuake"     , "Select Color$"  , "kruler"      , "kcolorchooser" , "Paste Special"  ,
+    "New Form"    , "Insert Picture" , "kcharselect" , "mythfrontend"  , "plasmoidviewer" ,
     "Pavucontrol"
 }
 
 -- Make the matching clients (by classes) on top of the default layout
 tyrannical.properties.ontop = {
-    "Xephyr"       , "ksnapshot"       , "kruler"     , "Qalculate-gtk", "Pavucontrol"
+    "Xephyr" , "ksnapshot" , "kruler" , "Qalculate-gtk" , "Pavucontrol"
 }
 
 -- Force the matching clients (by classes) to be centered on the screen on init
 tyrannical.properties.centered = {
-    "kcalc", "Qalculate-gtk", "Pavucontrol"
+    "kcalc" , "Qalculate-gtk" , "Pavucontrol"
 }
 
-tyrannical.settings.block_children_focus_stealing = false --Block popups ()
-tyrannical.settings.group_children = true --Force popups/dialogs to have the same tags as the parent client
+tyrannical.settings.block_children_focus_stealing = false -- Block popups ()
+tyrannical.settings.group_children = true                 -- Force popups/dialogs to have the same tags as the parent client
